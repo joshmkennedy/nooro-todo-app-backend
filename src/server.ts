bb52151cb558c32e1d201bbadaacbb7cfde59760
josh kennedy <josh@outthinkgroup.com>
2025-01-20 11:19:16 -0500

    routes scaffold

---
+++ src/server.ts
@@ -0,0 +1,47 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+//HOME
+app.get("/tasks", (req: Request, res: Response) => {
+	let tasks = [];
+	res.status(200).json({
+		success: true,
+		data: tasks,
+	});
+});
+
+//update tasks
+app.post("/tasks", (req: Request<any>, res: Response<any>): any => {
+	let tasks = [];
+	return res.status(201).json({
+		success: true,
+		data: tasks,
+	});
+});
+
+// update task
+app.put("/tasks/:id", (req: Request, res: Response): any => {
+	let task = {};
+
+	return res.status(200).json({
+		success: true,
+		data: task,
+	});
+});
+
+app.delete("/tasks/:id", (req: Request, res: Response): any => {
+
+	return res.status(200).json({
+		success: true,
+	});
+})
+
+// Start the server
+app.listen(port, () => {
+	console.log(`Server is running at http://localhost:${port}`);
+});
