63f696a78d76e3021566cc3b8974cade14a69f89
josh kennedy <josh@outthinkgroup.com>
2025-01-27 14:22:10 -0500

    Seed db with user

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,33 +1,51 @@
-import { PrismaClient } from '@prisma/client';
+import bcrypt from "bcrypt"
+import { PrismaClient, User } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 async function main() {
   // Clean up existing records (optional)
+  await prisma.user.deleteMany();
   await prisma.task.deleteMany();
+
+  const adminUserPass = "password";
+  const hashedPassword = await bcrypt.hash(adminUserPass, 10);
+	//@ts-ignore
+  const userData: User = {
+    email: "admin@email.com",
+    name: "Admin",
+    password: hashedPassword,
+  };
+
+  const user = await prisma.user.create({
+    data: userData,
+  });
 
   // Create sample tasks
   await prisma.task.createMany({
     data: [
       {
-        title: 'Buy groceries',
-        color: 'green',
+				userId: user.id,
+        title: "Buy groceries",
+        color: "green",
         completed: false,
       },
       {
-        title: 'Complete assignment',
-        color: 'yellow',
+				userId: user.id,
+        title: "Complete assignment",
+        color: "yellow",
         completed: false,
       },
       {
-        title: 'Review pull request',
-        color: 'blue',
+				userId: user.id,
+        title: "Review pull request",
+        color: "blue",
         completed: true,
       },
     ],
   });
 
-  console.log('Database has been seeded. 🌱');
+  console.log("Database has been seeded. 🌱");
 }
 
 main()
